9c5c2ebe30250a4a4fdd03c8ee1c57d733bc9198
Tobias Guse <tobe.guse@gmail.com>
2015-11-03 09:13:16 +0000

    added modal dialog

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('votingapp',['ui.bootstrap']);
 var counter = 1;
-app.controller('formCtrl',function($http,$scope){
-
+app.controller('formCtrl',function($http,$scope,$uibModal){
+  	$scope.items = ['item1', 'item2', 'item3'];
 	$scope.saveData = function(){
 	console.log($scope.form)
 	$http.post('/',$scope.form).then($scope.updateQuestions());
@@ -29,4 +29,33 @@ app.controller('formCtrl',function($http,$scope){
 $scope.close = function() {
  $scope.showModal = false;
 };
+$scope.open = function(size){
+	 var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: '/public/myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+}
+});
+
+angular.module('votingapp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 });
