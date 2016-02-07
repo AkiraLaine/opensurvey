1a4d37ed5281b4025e624341848181370f551134
Tobias Guse <tobe.guse@gmail.com>
2016-02-07 09:41:09 +0100

    environment variables

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //var keys = require(process.cwd()+'/keys.js');
-var mailgun = require('mailgun-js')({apiKey: mailgunKey, domain: mailgunDomain});
+var mailgun = require('mailgun-js')({apiKey: process.env.mailgunKey, domain: mailgunDomain});
 var mailcomposer = require('mailcomposer');
 module.exports = function (app, db, bcrypt,jwt,request,identicon,fmt) {
 
